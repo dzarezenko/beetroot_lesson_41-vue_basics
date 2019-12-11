3fbae287b02a83ff8e1e06405beaf3305cf28a42
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-11 20:14:31 +0200

    v-once

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div id="app">
-    
+  <div>
+    <h1 v-once>{{ title }}</h1>
+    <p>
+      {{ getTitle() }}
+    </p>
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      title: "Some title",
+      url: "https://google.com",
+    }
+  },
+  methods: {
+    getTitle() {
+      this.title = "Some other title";
+
+      return this.title;
+    },
+  },
 }
 </script>
