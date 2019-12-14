c16a940338fa2aeb58263b1dc740896d23814128
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-14 10:45:24 +0200

    Passing arguments with events

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ counter }}</h1>
     <form action="http://google.com">
-      <input type="submit" @mouseout="showAlert" @click.prevent="increaseCounter" value="Increase counter" />
+      <input type="submit" @click.prevent="increaseCounter(2, $event)" value="Increase counter" />
     </form>
   </div>
 </template>
@@ -18,10 +18,10 @@ export default {
     }
   },
   methods: {
-    increaseCounter(event) {
-      console.log(event);
+    increaseCounter(step, e) {
+      console.log(e);
 
-      this.counter++;
+      this.counter+= step;
       console.log(`Button clicked! Counter is ${this.counter}`);
     },
     showAlert() {
