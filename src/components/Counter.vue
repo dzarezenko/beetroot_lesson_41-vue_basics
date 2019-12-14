69ded951ff852bc6c9e35eefbfc4fe5c8146d980
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-14 10:52:53 +0200

    Event modifiers

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <h1>{{ counter }}</h1>
-    <form action="http://google.com">
-      <input type="submit" @click.prevent="increaseCounter(2, $event)" value="Increase counter" />
-    </form>
+    <button @click.once="increaseCounter(2, $event)">Increase counter</button>
   </div>
 </template>
 
 <script>
 // v-[directive]:[attribute].[modifier]
+
+/*
+
+.prevent - .preventDefault()
+.stop    - .stopPropagation()
+.once    - run the funciton at most once
+.self    - only trigger if the target of the event is itself
+
+*/
 
 export default {
   name: "Counter",
